376136def93c84a7b7378fc331ae4244bb0f60bf
Cryptic7 <112565113+Cryptic7X@users.noreply.github.com>
2026-05-18 20:19:03 +0530

    Update title from 'Kala' to 'Kal'

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Kala ⏳",
+  title: "Kal ⏳",
   description: "Count your remaining time based on average life expectancy",
   icons: {
     icon: '/icon.svg',
